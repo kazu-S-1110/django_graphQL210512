6108b8f8658cfc9caf3c9e5be2dd6b6251f3f459
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-14 12:37:47 +0900

    コードを修正

--- employees_test/schema.py
+++ employees_test/schema.py
@@ -7,7 +7,7 @@ class Query(users.schema.Query, api.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(users.schema.Query, api.schema.Query, graphene.ObjectType):
+class Mutation(users.schema.Mutation, api.schema.Mutation, graphene.ObjectType):
     pass
 
 
